82faee974b0eda2a572acd95f85ce281710d2acc
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2025-03-09 02:34:26 +0100

    feat: paginate without mapper

--- ids_std_sea/src/paginator/mod.rs
+++ ids_std_sea/src/paginator/mod.rs
@@ -6,7 +6,33 @@ use sea_orm::{ConnectionTrait, Paginator, SelectorTrait};
 
 use crate::convert::into::IntoDomain;
 
-pub async fn fetch_page<C, S, O, F>(
+pub async fn fetch_page<C, S>(
+    paginator: &Paginator<'_, C, S>,
+    query: &PaginationQuery,
+) -> Result<Page<S>, SelectRepoFailure>
+where
+    S: SelectorTrait<Item = S>,
+    C: ConnectionTrait,
+{
+    let page_info = paginator
+        .num_items_and_pages()
+        .await
+        .map_err(|err| err.into_domain())?;
+
+    let model_items: Vec<<S as SelectorTrait>::Item> = paginator
+        .fetch_page(query.page)
+        .await
+        .map_err(|err| err.into_domain())?;
+
+    Ok(Page {
+        data: model_items,
+        total: page_info.number_of_items,
+        page: page_info.number_of_pages,
+        page_size: query.page_size,
+    })
+}
+
+pub async fn fetch_page_mapper<C, S, O, F>(
     paginator: &Paginator<'_, C, S>,
     query: &PaginationQuery,
     mapper: F,
